bdf0fc97dbd50a3d838d7e0d870a99d24d3d81b2
allanferreirapinto <67429921+allanferreirapinto@users.noreply.github.com>
2021-08-20 09:55:26 -0300

    scripts-python

--- aula01/teste01.py
+++ aula01/teste01.py
@@ -1,9 +1,4 @@
 'Aulas de Python'
 "Comentário 2"
 '''Comentário 3
-A soma de dois números!
 '''
-numero1 = input('Digite o primeiro número: ')
-numero2 = input('Digite o segundo número: ')
-soma = numero1 + numero2
-print('A soma é', soma)
--- scripts-python/ex03.py
+++ scripts-python/ex03.py
@@ -1,3 +1,3 @@
-numero1 = int(input('Primeiro número '))
-numero2 = int(input('Segundo número '))
+numero1 = int(input('Digite primeiro número '))
+numero2 = int(input('Digite segundo número '))
 print('A soma é', numero1 + numero2)
