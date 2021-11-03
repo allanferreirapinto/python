65d7a5f3ff253e922e4b2cf7f084562a2b1b4a91
allanferreirapinto <67429921+allanferreirapinto@users.noreply.github.com>
2021-11-03 10:21:19 -0300

    aula16

---
+++ aula16/repeticao_while.py
@@ -0,0 +1,21 @@
+'''
+Estrutura de repetição for
+
+for i in range(0, 10):
+    print(i)
+print('Fim!')
+
+Estrutura de repetiçao while
+i = 0 # inicialização
+while i < 10:
+    print(i)
+    i = i + 1
+print('Fim!')
+'''
+
+a = 1
+while a != -1:
+    a = int(input('Entre com um número (-1 para sair): '))
+print('Fim')
+
+
