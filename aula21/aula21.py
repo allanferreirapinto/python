913d58819de9bf17ccf1c9d1127f78790ece497c
allanferreirapinto <67429921+allanferreirapinto@users.noreply.github.com>
2022-01-12 10:13:45 -0300

    aula21

---
+++ aula21/aula21.py
@@ -0,0 +1,26 @@
+'''
+d = {'nome': 'Joao', 'idade': 20, 'matricula': 1}
+for k in d:
+    print(k)
+
+d = {'nome': 'Joao', 'idade': 20, 'matricula': 1}
+for k in d.keys():
+    print(k)
+
+d = {'nome': 'Joao', 'idade': 20, 'matricula': 1}
+for v in d.values():
+    print(v)
+
+d = {'nome': 'Joao', 'idade': 20, 'matricula': 1}
+for k, v in d.items():
+    print(f'{k} = {v}')
+'''
+
+dados = dict()
+dados = {'nome': 'Matheus', 'idade': 18}
+print(dados['nome'])
+print(dados['idade'])
+dados['sexo'] = 'M'
+print(dados)
+del dados['idade']
+print(dados)
