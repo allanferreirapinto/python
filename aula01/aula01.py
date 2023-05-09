ec6b49633d86f1d440444764d96fdbaf5fc4bede
Allan Ferreira Pinto <67429921+allanferreirapinto@users.noreply.github.com>
2023-05-09 16:40:21 -0300

    Add files via upload

---
+++ aula01/aula01.py
@@ -0,0 +1,29 @@
+'''Aulas de Python
+nome = 'Felizberto'
+peso = 98.5
+idade = 82
+print(nome)
+print(peso)
+print(idade)
+2) Somar dois números inteiros
+num1 = int(input('Digite o primeiro número.: '))
+num2 = int(input('Digite o segundo número..: '))
+soma = num1 + num2
+print('A soma é', soma)
+
+3) Pergunta...
+'''
+num1 = float(input('Digite o primeiro número.: '))
+num2 = float(input('Digite o segundo número..: '))
+soma = num1 + num2
+print('A soma é', soma)
+
+# Comentário de uma linha
+
+'''
+Comentário 
+de 
+múltiplas
+linhas
+'''
+
